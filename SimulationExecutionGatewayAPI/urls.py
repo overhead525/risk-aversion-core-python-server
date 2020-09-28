6e3fa1ea152eb2a21bb700dd4c4f19f2870bdba3
overhead525 <ritchelcousar@outlook.com>
2020-09-28 14:02:29 -0400

    docs introduction done

--- SimulationExecutionGatewayAPI/urls.py
+++ SimulationExecutionGatewayAPI/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 from django.views.decorators.csrf import csrf_exempt
 
 from graphene_django.views import GraphQLView
@@ -22,6 +22,7 @@ from graphene_django.views import GraphQLView
 from schema import schema
 
 urlpatterns = [
+    path('', include('simulatorAPI.urls')),
     path('admin/', admin.site.urls),
     path('graphql', csrf_exempt(GraphQLView.as_view(graphiql=True, schema=schema)))
 ]
