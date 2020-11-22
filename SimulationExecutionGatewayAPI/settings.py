27e3140ae62b7c88dcb65b14fa0bfc2ba6bcf72a
overhead525 <overheadexistence@gmail.com>
2020-11-22 15:46:54 +0000

    repo is now ready to be used with aws from an arch linux system

--- SimulationExecutionGatewayAPI/settings.py
+++ SimulationExecutionGatewayAPI/settings.py
@@ -11,7 +11,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-import django_heroku
+from dotenv import load_dotenv
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -78,8 +79,12 @@ WSGI_APPLICATION = 'SimulationExecutionGatewayAPI.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USERNAME'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_ENDPOINT'),
+        'PORT': os.getenv('DB_PORT')
     }
 }
 
@@ -121,6 +126,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-
-# Django Setup for Heroku
-django_heroku.settings(locals())
